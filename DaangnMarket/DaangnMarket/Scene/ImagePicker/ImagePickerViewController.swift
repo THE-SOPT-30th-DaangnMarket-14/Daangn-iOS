83e50e71d6793b17bf7724078931f652eef7423b
dev-madilyn <lollin0@naver.com>
2022-05-14 16:28:18 +0900

    ➕[ADD] : New Project
    1. 프로젝트 생성
    2. Alamofire, RxSwift 설치
    3. Font Embed
    4. 폴더링
    5. Home, Writing, ImagePicker Group 생성
    6. Storyboard 생성

---
+++ DaangnMarket/DaangnMarket/Scene/ImagePicker/ImagePickerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImagePickerViewController.swift
+//  DaangnMarket
+//
+//  Created by madilyn on 2022/05/14.
+//
+
+import Foundation
+import UIKit
+
+class ImagePickerViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ DaangnMarket/DaangnMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaangnMarket
+//
+//  Created by madilyn on 2022/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
