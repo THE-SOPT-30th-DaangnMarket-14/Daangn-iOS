7061cd11c7035be10593806f19eb48a6daa37812
dev-madilyn <lollin0@naver.com>
2022-05-17 00:29:26 +0900

    ➕ [ADD]: UIColor+ 추가 #1

---
+++ DaangnMarket/DaangnMarket/Scene/Extension/UIColor+.swift
@@ -0,0 +1,55 @@
+//
+//  UIColor+.swift
+//  DaangnMarket
+//
+//  Created by madilyn on 2022/05/17.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var daangnBlack: UIColor {
+        return UIColor(red: 33.0 / 255.0, green: 36.0 / 255.0, blue: 41.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnBlue: UIColor {
+        return UIColor(red: 75.0 / 255.0, green: 106.0 / 255.0, blue: 234.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnGray00: UIColor {
+        return UIColor(red: 239.0 / 255.0, green: 239.0 / 255.0, blue: 239.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnGray01: UIColor {
+        return UIColor(red: 221.0 / 255.0, green: 221.0 / 255.0, blue: 221.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnGray02: UIColor {
+        return UIColor(red: 209.0 / 255.0, green: 211.0 / 255.0, blue: 215.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnGray03: UIColor {
+        return UIColor(red: 196.0 / 255.0, green: 196.0 / 255.0, blue: 196.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnGray04: UIColor {
+        return UIColor(red: 134.0 / 255.0, green: 135.0 / 255.0, blue: 139.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnGray05: UIColor {
+        return UIColor(red: 81.0 / 255.0, green: 82.0 / 255.0, blue: 86.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnIvory: UIColor {
+        return UIColor(red: 251.0 / 255.0, green: 248.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnOrange: UIColor {
+        return UIColor(red: 238.0 / 255.0, green: 133.0 / 255.0, blue: 72.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var daangnWhite: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0, alpha: 1.0)
+    }
+}
