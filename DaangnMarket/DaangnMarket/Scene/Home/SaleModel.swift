7d12677da6ee36e891da8ce7976a09f4cf84b379
dev-madilyn <lollin0@naver.com>
2022-05-17 22:31:25 +0900

    ➕ [ADD]: SaleModel 생성 #4

---
+++ DaangnMarket/DaangnMarket/Scene/Home/SaleModel.swift
@@ -0,0 +1,16 @@
+//
+//  SaleModel.swift
+//  DaangnMarket
+//
+//  Created by madilyn on 2022/05/17.
+//
+
+import Foundation
+
+struct SaleModel: Codable {
+    var imageName: String
+    var titleName: String
+    var localName: String
+    var updateTime: Int
+    var price: Int
+}
