a770101d90e6c90515078e10f5f334d4ada32cd7
dev-madilyn <lollin0@naver.com>
2022-05-17 23:14:52 +0900

    ✅ [CHORE]: dummy data 작성 #4

--- DaangnMarket/DaangnMarket/Scene/Home/HomeViewController.swift
+++ DaangnMarket/DaangnMarket/Scene/Home/HomeViewController.swift
@@ -15,6 +15,17 @@ class HomeViewController: UIViewController {
         }
     }
     
+    var saleDummyData = [
+        SaleModel(imageName: "iosImgHomeList", titleName: "폰케팔아염", localName: "서림동", updateTime: 3, price: 10000),
+        SaleModel(imageName: "iosImgHomeList", titleName: "폰케팔아염", localName: "서림동", updateTime: 2, price: 333333),
+        SaleModel(imageName: "iosImgHomeList", titleName: "폰케팔아염", localName: "서림동", updateTime: 1, price: 1004400),
+        SaleModel(imageName: "iosImgHomeList", titleName: "폰케팔아염", localName: "서림동", updateTime: 3, price: 10000),
+        SaleModel(imageName: "iosImgHomeList", titleName: "폰케팔아염", localName: "서림동", updateTime: 3, price: 10000),
+        SaleModel(imageName: "iosImgHomeList", titleName: "폰케팔아염", localName: "서림동", updateTime: 3, price: 10000),
+        SaleModel(imageName: "iosImgHomeList", titleName: "폰케팔아염", localName: "서림동", updateTime: 3, price: 10000),
+        SaleModel(imageName: "iosImgHomeList", titleName: "폰케팔아염", localName: "서림동", updateTime: 3, price: 10000)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
