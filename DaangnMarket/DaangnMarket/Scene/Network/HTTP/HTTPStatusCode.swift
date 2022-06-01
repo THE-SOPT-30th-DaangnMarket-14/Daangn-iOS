80ee80259ef315a6e43e2676c33cde200d278684
Jihyun247 <wlguslove12345@gmail.com>
2022-06-01 09:32:12 +0000

    HTTPHeaderField & HTTPStatusCode 헤더와 status code

---
+++ DaangnMarket/DaangnMarket/Scene/Network/HTTP/HTTPStatusCode.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPStatusCode.swift
+//  DaangnMarket
+//
+//  Created by 김지현 on 2022/06/03.
+//
+
+import Foundation
+
+enum HTTPStatusCode: Int {
+    case OK = 200
+    case NO_VALUE = 400
+    case SERVER_ERROR = 500
+}
