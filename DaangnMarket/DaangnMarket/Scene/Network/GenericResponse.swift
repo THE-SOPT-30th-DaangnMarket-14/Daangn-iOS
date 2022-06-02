49ec1191cc8f34c5ebfb8ce7b34f0905136b9e8b
dev-madilyn <lollin0@naver.com>
2022-06-02 20:57:25 +0900

    ➕ [ADD]: data 없는 Generic response 추가 #36

--- DaangnMarket/DaangnMarket/Scene/Network/GenericResponse.swift
+++ DaangnMarket/DaangnMarket/Scene/Network/GenericResponse.swift
@@ -29,3 +29,23 @@ struct GenericResponse<T: Codable>: Codable {
         data = (try? values.decode(T.self, forKey: .data)) ?? nil
     }
 }
+
+struct GenericNoDataResponse: Codable {
+    
+    var status: Int
+    var success: Bool
+    var message: String
+    
+    enum CodingKeys: String, CodingKey {
+        case status = "status"
+        case success = "success"
+        case message = "message"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        status = (try? values.decode(Int.self, forKey: .status)) ?? -1
+        success = (try? values.decode(Bool.self, forKey: .success)) ?? false
+        message = (try? values.decode(String.self, forKey: .message)) ?? ""
+    }
+}
