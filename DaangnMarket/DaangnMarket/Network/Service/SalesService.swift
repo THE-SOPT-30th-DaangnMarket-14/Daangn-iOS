e60e42ecf4e27dc9146b3cc4a9f104d6bf7cf431
LimYunHwi <88yhtserof@naver.com>
2022-06-02 22:59:51 +0900

    ✨ [FEAT]: 판매글 작성 Service 생성 #54

--- DaangnMarket/DaangnMarket/Network/Service/SalesService.swift
+++ DaangnMarket/DaangnMarket/Network/Service/SalesService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Alamofire
+import UIKit
 
 class SalesService {
     //MARK: - Properties
@@ -31,6 +32,45 @@ class SalesService {
                 completion(networkResult)
             case .failure(_):
                 completion(.networkFail)
+            }
+        }
+    }
+    
+    func addSalesPost(title: String,
+                      price: Int,
+                      contents: String,
+                      images: [UIImage],
+                      compleion: @escaping (NetworkResult<Any>) -> Void
+    ){
+        let url = APIConstants.addSalesPostURL
+        let headers: HTTPHeaders = ["Content-Type" : "multipart/form-data"]
+        let body: Parameters = [
+            "title" : title,
+            "price" : price,
+            "contents" : contents
+        ]
+        
+        let request = AF.upload(multipartFormData: { multipartFormData in
+            for (key, value) in body {
+                multipartFormData.append("\(value)".data(using: .utf8)!, withName: key)
+            }
+            images.forEach{ image in
+                if let imageData = image.pngData() {
+                    print("\(imageData).png")
+                    multipartFormData.append(imageData, withName: "\(imageData).png", fileName: "\(imageData).png", mimeType: "image/png")
+                }
+            }
+        }, to: url, usingThreshold: UInt64.init(), method: .post, headers: headers)
+        
+        request.responseData{ [weak self] response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else {return}
+                guard let value = response.value else {return}
+                guard let networkResult = self?.checkStatus(statusCode, data: value) else {return}
+                compleion(networkResult)
+            case .failure(_):
+                compleion(.networkFail)
             }
         }
     }
