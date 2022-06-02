e1167237018214bcf2dc7d487e03c93594bb09a3
LimYunHwi <88yhtserof@naver.com>
2022-06-02 14:27:56 +0900

    ✨ [FEAT]: 판매글 작성 요청 데이터 모델 생성 #54

---
+++ DaangnMarket/DaangnMarket/Model/WrtingSalesPost.swift
@@ -0,0 +1,15 @@
+//
+//  WrtingSalesPost.swift
+//  DaangnMarket
+//
+//  Created by 임윤휘 on 2022/06/02.
+//
+
+import Foundation
+
+struct WritingSalesPost {
+    let title: String
+    let price: Int
+    let contents: String
+    let image: Data
+}
