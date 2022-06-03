25b05c4f10b3330bdb07d6cf4dc54e80efdc1804
Jihyun247 <wlguslove12345@gmail.com>
2022-06-03 16:00:54 +0900

    APIConstatns 위치 변경

---
+++ DaangnMarket/DaangnMarket/Scene/Network/API/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  DaangnMarket
+//
+//  Created by 김지현 on 2022/05/31.
+//
+
+import Foundation
+
+struct APIConstants {
+    // MARK: - Base URL
+    static let baseURL = "http://52.78.114.176:8000"
+    static let itemPath = "/item"
+}
